1cd759ba6b9635c3fcab39ec1e25e05aa57bf51d
Muhammad Anas Imtiaz <anasimtiaz200899@gmail.com>
2021-10-01 22:27:28 +0500

    Implement Asp.Net.Core.Identity

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,6 +12,8 @@ using Application.Core;
 using API.Extensions;
 using FluentValidation.AspNetCore;
 using API.Middleware;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace API
 {
@@ -28,11 +30,17 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers().AddFluentValidation(_config => 
+            services.AddControllers(opt => 
+            {
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                opt.Filters.Add(new AuthorizeFilter(policy));
+            })
+                .AddFluentValidation(_config => 
             {
                 _config.RegisterValidatorsFromAssemblyContaining<Create>();
             });
-            services.AddApplicationServices(_config)
+            services.AddApplicationServices(_config);
+            services.AddIdentityServices(_config);
 ;        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +57,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
